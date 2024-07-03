52bbcfc431fd436e245fe9002b04240331b92f37
hashkitti001 <iJeddy5.0@gmail.com>
2024-07-03 19:21:49 +0100

    add Landing Page

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Explore a World of Ideas and Insights</h1>
+            <p className="py-6 text-md text-left">
+            Welcome to Glory's Blog, your gateway to thought-provoking articles, insightful perspectives, and engaging discussions. Dive into topics ranging from technology trends to personal development, crafted to inspire and inform. Join our community of curious minds and embark on a journey of discovery."
+            </p>
+            <button className="btn bg-green-400 text-white">Get Started</button>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Landing

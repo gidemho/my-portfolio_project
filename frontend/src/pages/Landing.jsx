8a580f5ae4fd76c87d9f3f5f59247af6ac1a5f0f
hashkitti001 <iJeddy5.0@gmail.com>
2024-07-04 01:03:15 +0100

    minor changes

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Explore a World of Ideas and Insights</h1>
             <p className="py-6 text-md text-left">
-            Welcome to Glory's Blog, your gateway to thought-provoking articles, insightful perspectives, and engaging discussions. Dive into topics ranging from technology trends to personal development, crafted to inspire and inform. Join our community of curious minds and embark on a journey of discovery."
+            Welcome to Glory's Blog, your gateway to thought-provoking articles, insightful perspectives, and engaging discussions. Dive into topics ranging from technology trends to personal development, crafted to inspire and inform. Join our community of curious minds and embark on a journey of discovery.
             </p>
             <button className="btn bg-green-400 text-white">Get Started</button>
           </div>

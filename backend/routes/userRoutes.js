c68f3ae770eaf65e0878a56dd87bf6f2e4cdc203
hashkitti001 <iJeddy5.0@gmail.com>
2024-07-07 12:55:51 +0100

    add like reaction functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,17 @@
-const express = require("express")
-const userRouter = express.Router()
-const userController = require("../controllers/userController")
+const express = require("express");
+const userRouter = express.Router();
+const userController = require("../controllers/userController");
+const postController = require("../controllers/postController");
+const authMiddleware = require("../utils/authMiddleware");
 
-userRouter.post("/api/v1/auth/login", userController.loginUser)
-userRouter.get("/api/v1/auth/logout", userController.logOutUser)
-userRouter.get("/api/v1/user/profile/:id", userController.getProfile)
-module.exports = userRouter
+userRouter.post("/api/v1/auth/login", userController.loginUser);
+userRouter.get("/api/v1/auth/logout", userController.logOutUser);
+userRouter.get("/api/v1/user/profile/:id", userController.getProfile);
+
+/* Routes for posts */
+userRouter.post("/api/v1/user/post", authMiddleware, postController.createPost);
+userRouter.get("/api/v1/posts/:id", postController.getPostById);
+userRouter.get("/api/v1/posts/all", postController.getAllPosts);
+userRouter.post("/api/v1/posts/like/:id", postController.likePost);
+
+module.exports = userRouter;

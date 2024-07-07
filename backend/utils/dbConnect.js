48e34ccab884381c87e446a63ed78ffdde00daa3
hashkitti001 <iJeddy5.0@gmail.com>
2024-07-07 14:50:23 +0100

    mod to use prod db

--- backend/utils/dbConnect.js
+++ backend/utils/dbConnect.js
@@ -1,10 +1,21 @@
-require("dotenv").config()
-const mongoose = require("mongoose")
+require("dotenv").config();
+const mongoose = require("mongoose");
+
 /* 
 - A function to initiate connection to the MongoDB database
 */
 async function dbConnect() {
-    await mongoose.connect(process.env.DB_STRING)
-
+  try {
+    const dbString = `mongodb+srv://${process.env.PROD_DB_USERNAME}:${process.env.PROD_DB_PASSWORD}@cluster0.rmjefxt.mongodb.net`;
+    await mongoose.connect(dbString, {
+      dbName: "GBlog", // Replace with your actual database name
+      useNewUrlParser: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit process with failure
+  }
 }
-module.exports = dbConnect
+
+module.exports = dbConnect;
